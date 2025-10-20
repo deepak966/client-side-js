a1b597a8379c4259a726c6c98c1aa9fa264e4b14
deepak966 <deepakkatwal04@gmail.com>
2025-10-20 08:13:02 +0545

    feat: created updatebackgroundcolor function to to detect the slider change and change the background accordingly ..

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -2,3 +2,20 @@ const redSlider = document.getElementById('red');
 const greenSlider = document.getElementById('green');
 const blueSlider = document.getElementById('blue');
 const htmlElement = document.documentElement;
+
+// function to update backgroundColor
+function updateBackgroundColor(){
+    const redValue = redSlider.value;
+    const greenValue = greenSlider.value;
+    const blueValue = blueSlider.value;
+
+    //set the background color
+    htmlElement.style.backgroundColor = `rgb(${redValue},${greenValue},${blueValue})`;
+}
+
+//add event listener to detect slider change
+redSlider.addEventListener('input',updateBackgroundColor);
+greenSlider.addEventListener('input',updateBackgroundColor);
+blueSlider.addEventListener('input',updateBackgroundColor)
+
+updateBackgroundColor();
